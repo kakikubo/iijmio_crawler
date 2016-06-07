ff7fbd7dd0ae024f3d277fe50068a91813009d70
kakikubo teruo <kakikubo@gmail.com>
2016-06-07 21:50:25 +0900

    initial commit

---
+++ lib/iijmio_crawler/version.rb
@@ -0,0 +1,3 @@
+module IijmioCrawler
+  VERSION = "0.1.0"
+end
---
+++ spec/iijmio_crawler_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe IijmioCrawler do
+  it 'has a version number' do
+    expect(IijmioCrawler::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
